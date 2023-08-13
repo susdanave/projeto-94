95755f733beb68ba99e72154921c6a33abb4f746
susdanave <121106074+susdanave@users.noreply.github.com>
2023-08-13 10:35:56 -0300

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAbhwD58sOyrsBa1wV2EWlDFssD-0IwfUc",
+    authDomain: "projeto94-e82bb.firebaseapp.com",
+    databaseURL: "https://projeto94-e82bb-default-rtdb.firebaseio.com",
+    projectId: "projeto94-e82bb",
+    storageBucket: "projeto94-e82bb.appspot.com",
+    messagingSenderId: "290237222127",
+    appId: "1:290237222127:web:32f6ab0cf304b6aa9a1f1a",
+    measurementId: "G-65G0Z0V8TD"
+  };
